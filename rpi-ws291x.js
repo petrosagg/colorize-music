36456ba6d08936d80669e71c45237e1d7247c6b8
Petros Angelatos <petrosagg@gmail.com>
2018-09-01 00:41:27 -0700

    remove sigint handler
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>

--- rpi-ws291x.js
+++ rpi-ws291x.js
@@ -1,17 +1,10 @@
-var ws281x = require('rpi-ws281x-native/');
+var ws281x = require('rpi-ws281x-native');
 
 module.exports = function setColor(color) {
   var NUM_LEDS = 60,
   pixelData = new Uint32Array(NUM_LEDS);
 
   ws281x.init(NUM_LEDS);
-
-  // ---- trap the SIGINT and reset before exit
-  process.on('SIGINT', function () {
-    ws281x.reset();
-    process.nextTick(function () { process.exit(0); });
-  });
-
 
   for(var i = 0; i < NUM_LEDS; i++) {
       pixelData[i] = color;
